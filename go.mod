0c2c9cdcca2107e5b62d8f890d61875a78d67826
Diego Alzate <diego35.da@gmail.com>
2024-04-14 07:35:41 -0500

    initial setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diegoalzate/htmx-go-course
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Template struct {
+	tmpl *template.Template
+}
+
+func newTemplate() *Template {
+	return &Template{
+		tmpl: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	serverMux := http.NewServeMux()
+
+	count := Count{
+		Count: 0,
+	}
+
+	serverMux.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t := newTemplate()
+		count.Count++
+		if err := t.Render(w, "index.html", count); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	}))
+
+	server := http.Server{
+		Handler: serverMux,
+		Addr:    ":8080",
+	}
+
+	fmt.Print("Server is running on port 8080")
+	log.Fatal(server.ListenAndServe())
+
+}
