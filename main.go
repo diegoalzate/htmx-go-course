3345c6d33f5c7c73888507f9ddea1d1c119c15a9
Diego Alzate <diego35.da@gmail.com>
2024-04-16 07:03:06 -0500

    add form and contact htmx

--- main.go
+++ main.go
@@ -22,30 +22,54 @@ func (t *Template) Render(w io.Writer, name string, data interface{}) error {
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Email string
+	Name  string
+}
+
+func newContact(email, name string) Contact {
+	return Contact{
+		Email: email,
+		Name:  name,
+	}
+}
+
+type AppState struct {
+	Contacts []Contact
+}
+
+func newAppState() AppState {
+	return AppState{
+		Contacts: []Contact{
+			newContact("diego@gmail.com", "diego"),
+			newContact("bob@gmail.com", "bob"),
+		},
+	}
 }
 
 func main() {
 	serverMux := http.NewServeMux()
 
-	count := Count{
-		Count: 0,
-	}
+	state := newAppState()
 
 	t := newTemplate()
 
 	serverMux.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := t.Render(w, "index", count); err != nil {
+		if err := t.Render(w, "index", state); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}))
 
-	serverMux.Handle("POST /count", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hit")
-		count.Count += 1
-		if err := t.Render(w, "count", count); err != nil {
+	serverMux.Handle("POST /contacts", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		email := r.FormValue("email")
+		name := r.FormValue("name")
+
+		contact := newContact(email, name)
+
+		state.Contacts = append(state.Contacts, contact)
+
+		if err := t.Render(w, "contacts", state); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
